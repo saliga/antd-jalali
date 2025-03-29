e936cef374b423c373ef8a10450a01cfdff41394
Ali Ghadiri <ghadirinia1373@gmail.com>
2025-03-30 02:22:14 +0330

    react-script build

--- lib/types/calendar/Calendar.d.ts
+++
@@ -1,4 +0,0 @@
-import { Dayjs } from "dayjs";
-declare const Calendar: (props: import("antd/es/calendar/generateCalendar").CalendarProps<Dayjs>) => import("react").JSX.Element;
-export default Calendar;
-//# sourceMappingURL=Calendar.d.ts.map
